7875ebf45c3d958c4e26e5d29800f05b9837f6c7
Pedrotudou <pedroicoppola@gmail.com>
2025-11-26 21:08:52 -0300

    Intenté hacer la app PWA

--- header.php
+++ header.php
@@ -2,6 +2,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="manifest" href="/manifest.json">
+  <meta name="theme-color" content="#2563eb">
+  <script>
+    if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("✅ Service Worker registrado:", reg.scope))
+      .catch((err) => console.log("❌ Error registrando SW:", err));
+  });
+}
+
+  </script>
 </head>
 
 <header class="flex justify-between items-center px-4 py-3 border-b border-gray-200 bg-white sticky top-0 z-50">
